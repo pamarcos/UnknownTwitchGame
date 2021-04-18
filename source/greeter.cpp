279e5f3ade30936455a28c600b0a04e409c86628
Pablo Marcos <pablo.marcos.oltra@gmail.com>
2021-04-18 19:56:42 +0200

    Modify .clang-format and files accordingly

--- source/greeter.cpp
+++ source/greeter.cpp
@@ -6,15 +6,15 @@ using namespace greeter;
 Greeter::Greeter(std::string _name) : name(std::move(_name)) {}
 
 std::string Greeter::greet(LanguageCode lang) const {
-  switch (lang) {
+    switch (lang) {
     default:
     case LanguageCode::EN:
-      return fmt::format("Hello, {}!", name);
+        return fmt::format("Hello, {}!", name);
     case LanguageCode::DE:
-      return fmt::format("Hallo {}!", name);
+        return fmt::format("Hallo {}!", name);
     case LanguageCode::ES:
-      return fmt::format("¡Hola {}!", name);
+        return fmt::format("¡Hola {}!", name);
     case LanguageCode::FR:
-      return fmt::format("Bonjour {}!", name);
-  }
+        return fmt::format("Bonjour {}!", name);
+    }
 }
--- test/source/greeter.cpp
+++ test/source/greeter.cpp
@@ -5,17 +5,17 @@
 #include <string>
 
 TEST_CASE("Greeter") {
-  using namespace greeter;
+    using namespace greeter;
 
-  Greeter greeter("Tests");
+    Greeter greeter("Tests");
 
-  CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
-  CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
-  CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
-  CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
+    CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
+    CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
+    CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
+    CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
 }
 
 TEST_CASE("Greeter version") {
-  static_assert(std::string_view(GREETER_VERSION) == std::string_view("1.0"));
-  CHECK(std::string(GREETER_VERSION) == std::string("1.0"));
+    static_assert(std::string_view(GREETER_VERSION) == std::string_view("1.0"));
+    CHECK(std::string(GREETER_VERSION) == std::string("1.0"));
 }
